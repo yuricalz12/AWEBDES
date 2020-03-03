0fe8192f1abbe6076edce7d86130f32bb91e1892
unknown <yurelle624@gmail.com>
2020-03-03 08:11:37 +0800

    finish naming convention

--- awebdes.sql
+++ awebdes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 02, 2020 at 02:25 AM
+-- Generation Time: Mar 03, 2020 at 01:09 AM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.8
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `course` (
-  `id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
   `department_id` int(11) NOT NULL,
   `course_name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -38,7 +38,7 @@ CREATE TABLE `course` (
 -- Dumping data for table `course`
 --
 
-INSERT INTO `course` (`id`, `department_id`, `course_name`) VALUES
+INSERT INTO `course` (`course_id`, `department_id`, `course_name`) VALUES
 (1, 1, 'Course1'),
 (2, 2, 'Course2');
 
@@ -49,7 +49,7 @@ INSERT INTO `course` (`id`, `department_id`, `course_name`) VALUES
 --
 
 CREATE TABLE `department` (
-  `id` int(11) NOT NULL,
+  `department_id` int(11) NOT NULL,
   `department_name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -57,7 +57,7 @@ CREATE TABLE `department` (
 -- Dumping data for table `department`
 --
 
-INSERT INTO `department` (`id`, `department_name`) VALUES
+INSERT INTO `department` (`department_id`, `department_name`) VALUES
 (1, 'Department1'),
 (2, 'Department2');
 
@@ -68,7 +68,7 @@ INSERT INTO `department` (`id`, `department_name`) VALUES
 --
 
 CREATE TABLE `room` (
-  `id` int(11) NOT NULL,
+  `room_id` int(11) NOT NULL,
   `room_name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -76,7 +76,7 @@ CREATE TABLE `room` (
 -- Dumping data for table `room`
 --
 
-INSERT INTO `room` (`id`, `room_name`) VALUES
+INSERT INTO `room` (`room_id`, `room_name`) VALUES
 (1, 'Room 111'),
 (2, 'Room 101');
 
@@ -87,30 +87,35 @@ INSERT INTO `room` (`id`, `room_name`) VALUES
 --
 
 CREATE TABLE `schedule` (
-  `id` int(11) NOT NULL,
+  `schedule_id` int(11) NOT NULL,
   `subject_id` int(11) NOT NULL,
   `room_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `dpd_id` int(11) NOT NULL,
-  `type` varchar(50) NOT NULL,
+  `class_type` varchar(50) NOT NULL,
   `day` varchar(50) NOT NULL,
-  `start_time` int(11) NOT NULL,
-  `end_time` int(11) NOT NULL
+  `start_time_id` int(11) NOT NULL,
+  `end_time_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `schedule`
 --
 
-INSERT INTO `schedule` (`id`, `subject_id`, `room_id`, `user_id`, `dpd_id`, `type`, `day`, `start_time`, `end_time`) VALUES
-(4, 1, 1, 15, 17, 'lecture', 'monday', 1, 3),
+INSERT INTO `schedule` (`schedule_id`, `subject_id`, `room_id`, `user_id`, `dpd_id`, `class_type`, `day`, `start_time_id`, `end_time_id`) VALUES
 (6, 1, 1, 15, 17, 'laboratory', 'monday', 7, 9),
-(9, 1, 1, 15, 17, 'laboratory', 'monday', 11, 15),
 (10, 1, 1, 15, 17, 'lecture', 'monday', 3, 5),
 (13, 2, 1, 15, 17, 'laboratory', 'monday', 15, 17),
 (14, 1, 2, 16, 17, 'lecture', 'monday', 1, 5),
 (15, 2, 2, 16, 17, 'lecture', 'monday', 5, 11),
-(16, 2, 1, 15, 17, 'lecture', 'monday', 17, 23);
+(16, 2, 1, 15, 17, 'lecture', 'monday', 17, 23),
+(17, 1, 1, 15, 17, 'lecture', 'monday', 1, 3),
+(18, 1, 1, 15, 17, 'lecture', 'monday', 1, 3),
+(19, 1, 1, 15, 17, 'lecture', 'monday', 1, 3),
+(20, 1, 1, 15, 17, 'lecture', 'monday', 1, 3),
+(21, 1, 1, 15, 17, 'lecture', 'monday', 1, 3),
+(22, 2, 1, 16, 17, 'laboratory', 'monday', 11, 13),
+(23, 1, 2, 16, 17, 'laboratory', 'monday', 13, 15);
 
 -- --------------------------------------------------------
 
@@ -119,7 +124,7 @@ INSERT INTO `schedule` (`id`, `subject_id`, `room_id`, `user_id`, `dpd_id`, `typ
 --
 
 CREATE TABLE `section` (
-  `id` int(11) NOT NULL,
+  `section_id` int(11) NOT NULL,
   `section_name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -130,23 +135,23 @@ CREATE TABLE `section` (
 --
 
 CREATE TABLE `subject` (
-  `id` int(11) NOT NULL,
+  `subject_id` int(11) NOT NULL,
   `subject_name` varchar(50) NOT NULL,
   `subject_code` varchar(50) NOT NULL,
-  `lecture_hour` int(11) NOT NULL,
-  `lab_hour` int(11) NOT NULL,
-  `department` int(11) NOT NULL,
-  `course` int(11) NOT NULL,
-  `sem` int(11) NOT NULL,
-  `year_level` int(11) NOT NULL,
-  `color` varchar(50) NOT NULL
+  `subject_lecture_hour` int(11) NOT NULL,
+  `subject_lab_hour` int(11) NOT NULL,
+  `department_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `semester_id` int(11) NOT NULL,
+  `year_level_id` int(11) NOT NULL,
+  `subject_color` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `subject`
 --
 
-INSERT INTO `subject` (`id`, `subject_name`, `subject_code`, `lecture_hour`, `lab_hour`, `department`, `course`, `sem`, `year_level`, `color`) VALUES
+INSERT INTO `subject` (`subject_id`, `subject_name`, `subject_code`, `subject_lecture_hour`, `subject_lab_hour`, `department_id`, `course_id`, `semester_id`, `year_level_id`, `subject_color`) VALUES
 (1, 'AWEBDES', 'AWEBDES', 2, 3, 1, 1, 1, 1, '#1c69e6'),
 (2, 'INFOMAN', 'INFOMAN', 3, 3, 1, 1, 1, 1, '#07f22e');
 
@@ -157,7 +162,7 @@ INSERT INTO `subject` (`id`, `subject_name`, `subject_code`, `lecture_hour`, `la
 --
 
 CREATE TABLE `time` (
-  `id` int(11) NOT NULL,
+  `time_id` int(11) NOT NULL,
   `time` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -165,7 +170,7 @@ CREATE TABLE `time` (
 -- Dumping data for table `time`
 --
 
-INSERT INTO `time` (`id`, `time`) VALUES
+INSERT INTO `time` (`time_id`, `time`) VALUES
 (1, '7:00 am'),
 (2, '7:30 am'),
 (3, '8:00 am'),
@@ -203,19 +208,19 @@ INSERT INTO `time` (`id`, `time`) VALUES
 --
 
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `email` varchar(250) NOT NULL,
-  `password` varchar(250) NOT NULL,
-  `department` int(11) NOT NULL,
-  `course` int(11) NOT NULL,
-  `type` int(11) NOT NULL COMMENT '1 = student'
+  `user_id` int(11) NOT NULL,
+  `user_email` varchar(250) NOT NULL,
+  `user_password` varchar(250) NOT NULL,
+  `department_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `user_type` int(11) NOT NULL COMMENT '1 = student'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `email`, `password`, `department`, `course`, `type`) VALUES
+INSERT INTO `user` (`user_id`, `user_email`, `user_password`, `department_id`, `course_id`, `user_type`) VALUES
 (15, 'yurelle624@gmail.com', '$2y$10$BEYL/.FLPP6J5UdPRngkvO/wmg3XCdT0XwyM7rc4806kMqmLwn3yu', 1, 1, 1),
 (16, 'johndoe@gmail.com', '$2y$10$mbKiyg3hzM0Q7irekW2gXe2yCy.MM9hsgRFYGiE4eRKQZ4wS4Md2.', 1, 0, 2),
 (17, 'janedoe@gmail.com', '$2y$10$rcgr6DrCjxF6LWh8GUQLbeJ5yYcByJXvb0dWGtcWbcL0b3aa8xu1e', 2, 0, 3),
@@ -228,21 +233,21 @@ INSERT INTO `user` (`id`, `email`, `password`, `department`, `course`, `type`) V
 --
 
 CREATE TABLE `user_information` (
-  `id` int(11) NOT NULL,
+  `info_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `first_name` varchar(250) NOT NULL,
-  `last_name` varchar(250) NOT NULL,
-  `address` varchar(250) NOT NULL,
-  `gender` varchar(250) NOT NULL,
-  `dob` date NOT NULL,
-  `profile_picture` varchar(250) NOT NULL
+  `info_first_name` varchar(250) NOT NULL,
+  `info_last_name` varchar(250) NOT NULL,
+  `info_address` varchar(250) NOT NULL,
+  `info_gender` varchar(250) NOT NULL,
+  `info_dob` date NOT NULL,
+  `info_profile_picture` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user_information`
 --
 
-INSERT INTO `user_information` (`id`, `user_id`, `first_name`, `last_name`, `address`, `gender`, `dob`, `profile_picture`) VALUES
+INSERT INTO `user_information` (`info_id`, `user_id`, `info_first_name`, `info_last_name`, `info_address`, `info_gender`, `info_dob`, `info_profile_picture`) VALUES
 (6, 15, 'Judge Yuri', 'Calumpang', 'Gomez St.', 'Male', '1997-01-01', 'img/Male.png'),
 (7, 16, 'John', 'Doe', 'Dumaguete', 'Male', '1995-04-04', 'img/Male.png'),
 (8, 17, 'Jane', 'Doe', 'Dumaguete', 'Female', '1997-03-21', 'img/Female.png'),
@@ -256,55 +261,55 @@ INSERT INTO `user_information` (`id`, `user_id`, `first_name`, `last_name`, `add
 -- Indexes for table `course`
 --
 ALTER TABLE `course`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`course_id`);
 
 --
 -- Indexes for table `department`
 --
 ALTER TABLE `department`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`department_id`);
 
 --
 -- Indexes for table `room`
 --
 ALTER TABLE `room`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`room_id`);
 
 --
 -- Indexes for table `schedule`
 --
 ALTER TABLE `schedule`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`schedule_id`);
 
 --
 -- Indexes for table `section`
 --
 ALTER TABLE `section`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`section_id`);
 
 --
 -- Indexes for table `subject`
 --
 ALTER TABLE `subject`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`subject_id`);
 
 --
 -- Indexes for table `time`
 --
 ALTER TABLE `time`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`time_id`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`user_id`);
 
 --
 -- Indexes for table `user_information`
 --
 ALTER TABLE `user_information`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`info_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -314,55 +319,55 @@ ALTER TABLE `user_information`
 -- AUTO_INCREMENT for table `course`
 --
 ALTER TABLE `course`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `department`
 --
 ALTER TABLE `department`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `department_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `room`
 --
 ALTER TABLE `room`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `room_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `schedule`
 --
 ALTER TABLE `schedule`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `schedule_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `section`
 --
 ALTER TABLE `section`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `section_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `subject`
 --
 ALTER TABLE `subject`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `time`
 --
 ALTER TABLE `time`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `time_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `user_information`
 --
 ALTER TABLE `user_information`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `info_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
